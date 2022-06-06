5c02df33cc9ee7718a7daede58db1f470b544da3
Achintya Sharma <achintya9998@gmail.com>
2022-06-07 01:47:22 +0530

    DSA

--- LinkedList/DoublyLinkedList.cpp
+++ LinkedList/DoublyLinkedList.cpp
@@ -14,6 +14,18 @@ class Node{
             this->next = NULL;
         }
 
+        //Deconstructor
+        ~Node()
+        {
+            int val = this->data;
+            if (next != NULL)
+            {
+                delete next;
+                next = NULL;
+            }
+            cout<<"Memory Freed for Node with data "<<val<<endl;
+        }
+
 };
 
 int getLength(Node* head)            // Gives Length of the Linked List
@@ -28,20 +40,98 @@ int getLength(Node* head)            // Gives Length of the Linked List
     return len;
 }
 
-void insertAtHead(Node* &head, int d)   // Insertion at Head
+void insertAtHead(Node* &head, Node* &tail, int d)   // Insertion at Head
 {
-    Node* temp = new Node(d);
-    temp->next = head;
-    head->prev = temp;
-    head = temp;
+    if (head == NULL)
+    {
+        Node* temp = new Node(d);
+        head = temp;
+        tail = temp;
+    }
+    else
+    {
+        Node* temp = new Node(d);
+        temp->next = head;
+        head->prev = temp;
+        head = temp;
+    }
 }
 
-void insertAtTail(Node* &tail, int d)  // Insertion at Tail
+void insertAtTail(Node* &tail, Node* &head, int d)  // Insertion at Tail
 {
-    Node* temp = new Node(d);
-    tail->next = temp;
-    temp->prev = tail;
-    tail = temp;
+    if (tail == NULL)
+    {
+        Node* temp = new Node(d);
+        tail = temp;
+        head = temp;
+    }
+    else
+    {
+        Node* temp = new Node(d);
+        tail->next = temp;
+        temp->prev = tail;
+        tail = temp;
+    }
+}
+
+void insertAtPosition(Node* &head, Node* &tail,int position, int d)
+{
+    if (position == 1)
+    {
+        insertAtHead(head, tail, d);
+        return;
+    }
+    else
+    {
+        Node* temp = head;           // temp is the node which is one node behind the node which is going to get add
+        int cnt = 1;
+        while(cnt < position - 1)
+        {
+            temp = temp->next;
+            cnt++;
+        }
+        
+        if (temp->next == NULL)
+        {
+            insertAtTail(tail, head, d);
+            return;
+        }
+        Node* nodeToInsert = new Node(d);
+
+        nodeToInsert->next = temp->next;
+        temp->next->prev = nodeToInsert;
+        temp->next = nodeToInsert;
+        nodeToInsert->prev = temp;
+    }
+}
+
+void deleteNode(Node* &head, Node* &tail, int position)
+{
+    if (position == 1)
+    {
+        Node* temp = head;
+        temp->next->prev = NULL;
+        head = temp->next;
+        temp->next = NULL;
+        delete temp;
+    }
+    else
+    {
+        Node* curr = head;
+        Node* prev = NULL;
+
+        int cnt = 1;
+        while(cnt < position)
+        {
+            prev = curr;
+            curr = curr->next;
+            cnt++;
+        }
+        curr->prev = NULL;
+        prev->next = curr->next;
+        curr->next = NULL;
+        delete curr;
+    }
 }
 
 void print(Node* &head)                // Traversal of Doubly Linked List
@@ -56,16 +146,27 @@ void print(Node* &head)                // Traversal of Doubly Linked List
 }
 int main()
 {
-    Node* node1 = new Node(10);
-    Node* head = node1;
-    Node* tail  = node1;
+    //Node* node1 = new Node(10);
+    Node* head = NULL;
+    Node* tail  = NULL;
 
+    //print(head);
+    //cout<<getLength(head)<<endl;
+
+    insertAtHead(head, tail, 20);
+    insertAtHead(head, tail, 30);
+    insertAtTail(tail, head, 50);
+    insertAtPosition(head, tail, 2, 100);
+    insertAtPosition(head, tail, 4, 200);
+    insertAtTail(tail, head, 450);
+    cout<<"Linked List before Deletion: "<<endl;
     print(head);
-    cout<<getLength(head)<<endl;
 
-    insertAtHead(head, 20);
-    insertAtHead(head, 30);
-    insertAtTail(tail, 50);
+    deleteNode(head, tail, 2);
 
+    cout<<"Linked List after Deletion: "<<endl;
     print(head);
+    
+    cout<<" Head : "<<head->data<<endl;
+    cout<<" Tail : "<<tail->data;
 }
