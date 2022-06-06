b0bf585b24b922c7258dda4de24c48b12d8dac41
Achintya Sharma <achintya9998@gmail.com>
2022-06-06 20:59:07 +0530

    DSA

--- LinkedList/DoublyLinkedList.cpp
+++ LinkedList/DoublyLinkedList.cpp
@@ -28,6 +28,22 @@ int getLength(Node* head)            // Gives Length of the Linked List
     return len;
 }
 
+void insertAtHead(Node* &head, int d)   // Insertion at Head
+{
+    Node* temp = new Node(d);
+    temp->next = head;
+    head->prev = temp;
+    head = temp;
+}
+
+void insertAtTail(Node* &tail, int d)  // Insertion at Tail
+{
+    Node* temp = new Node(d);
+    tail->next = temp;
+    temp->prev = tail;
+    tail = temp;
+}
+
 void print(Node* &head)                // Traversal of Doubly Linked List
 {
     Node *temp = head;
@@ -42,7 +58,14 @@ int main()
 {
     Node* node1 = new Node(10);
     Node* head = node1;
+    Node* tail  = node1;
 
     print(head);
-    cout<<getLength(head);
+    cout<<getLength(head)<<endl;
+
+    insertAtHead(head, 20);
+    insertAtHead(head, 30);
+    insertAtTail(tail, 50);
+
+    print(head);
 }
