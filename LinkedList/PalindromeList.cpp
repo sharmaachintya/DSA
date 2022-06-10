458abd6d9f04a27193da9cfc2613b76c8552df3a
Achintya Sharma <achintya9998@gmail.com>
2022-06-11 03:17:42 +0530

    DSA

--- LinkedList/PalindromeList.cpp
+++ LinkedList/PalindromeList.cpp
@@ -55,11 +55,13 @@ void print(Node* &head)                  // L.L Traversal
 
 /////////////////////////////////////////////////////////////////////
 
+// Approach 1 (Works but causes Space Limit Exceeded) {S.C: O(N)}
+
 bool palindrome(Node* head)
 {
-    if (head == NULL)
+    if (head == NULL || head->next == NULL)
     {
-        return false;
+        return true;
     }
     vector<int> arr;
     Node* temp = head;
@@ -84,6 +86,61 @@ bool palindrome(Node* head)
     }
     return true;
 }
+
+// Approach 2 {S.C: O(1)}
+
+Node* getMid(Node* head)
+{
+    Node* slow = head;
+    Node* fast = head->next;
+    while (fast != NULL && fast->next != NULL)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    return slow;
+}
+
+Node* reverse(Node* head)
+{
+    Node* curr = head;
+    Node* prev = NULL;
+    Node* next = NULL;
+    while (curr != NULL)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+bool pallindrome2(Node* head)
+{
+    if (head == NULL || head->next == NULL)
+    {
+        return true;
+    }
+    Node* middle = getMid(head);            // Find Middle 
+    Node* temp = middle->next;              // Reverse L.L after the Middle
+    middle->next = reverse(temp);
+    Node* head1 = head;                     // Compare Both Halves
+    Node* head2 = middle->next;
+    while (head2 != NULL)
+    {
+        if (head1->data != head2->data)
+        {
+            return false;
+        }
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+    temp = middle->next;                   // Again Reversing the Other half. So, that the L.L remains intact
+    middle->next = reverse(temp);
+    return true;
+}
+
 
 ////////////////////////////////////////////////////////////////////
 
