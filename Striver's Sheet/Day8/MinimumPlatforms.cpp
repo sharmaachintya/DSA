e824a4314605508dcde99464942bef0d3ea09a93
Achintya Sharma <achintya9998@gmail.com>
2022-06-16 23:01:32 +0530

    Stack

---
+++ Striver's Sheet/Day8/MinimumPlatforms.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int minimumPlat(vector<int> start, vector<int> end)
+{
+    sort(start.begin(), start.end());
+    sort(end.begin(), end.end());
+    int n = start.size();
+    int plat = 1, max = 1;
+    int i = 1, j = 0;
+    while (i < n && j < n)
+    {
+        if (start[i] <= end[j])
+        {
+            plat++;
+            i++;
+        }
+        else if (start[i] > end[j])
+        {
+            plat--;
+            j++;
+        }
+        if (plat > max)
+        {
+            max = plat;
+        }
+    }
+    return max;
+}
+
+int main()
+{
+    vector<int> start = {120, 200, 700, 850, 50, 550};
+    vector<int> end = {550, 700, 1000, 500, 600, 900}; 
+
+    int ans = minimumPlat(start, end);  
+    cout<<ans;
+}
