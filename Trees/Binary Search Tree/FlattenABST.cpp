b7fea5c518168eaf8a5639f0dbfc59f5aed4c1ab
Achintya Sharma <achintya9998@gmail.com>
2022-06-27 15:34:15 +0530

    Binary Search Tree

---
+++ Trees/Binary Search Tree/FlattenABST.cpp
@@ -0,0 +1,133 @@
+
+#include<iostream>
+#include<queue>
+using namespace std;
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int d)
+        {
+            this->data = d;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+void levelOrderTrav(Node* root)
+{
+    queue<Node*> q;
+    if (root == NULL)
+    {
+        return;
+    }
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        Node* temp = q.front();
+        q.pop();
+
+        if (temp == NULL)
+        {
+            cout<<endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout<<temp->data<<" ";
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+Node* insertIntoBST(Node* &root, int d)
+{
+    if (root == NULL)
+    {
+        root = new Node(d);
+        return root;
+    }
+    if (d > root->data)
+    {
+        root->right = insertIntoBST(root->right, d);
+    }
+    else
+    {
+        root->left = insertIntoBST(root->left, d);
+    }
+    return root;
+}
+
+void takeInput(Node* &root)
+{
+    int data;
+    cin>>data;
+
+    while (data != -1)
+    {
+        insertIntoBST(root,data);
+        cin>>data;
+    }
+}
+
+//////////////////////////////////////////////////////////
+
+
+void inorderTrav(Node* root, vector<int> &ans)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inorderTrav(root->left, ans);
+    ans.push_back(root->data);
+    inorderTrav(root->right, ans);
+}
+
+Node* twoSumInBST(Node* root)
+{
+    vector<int> ans;
+    inorderTrav(root, ans);
+    Node* newRoot = new Node(ans[0]);
+    int size = ans.size();
+    Node* curr = newRoot;
+
+    for (int i=0;i<size;i++)
+    {
+        Node* temp = new Node(ans[i]);
+        curr->left = NULL;
+        curr->right = temp;
+        curr = temp;
+    }
+
+    curr->left = NULL;
+    curr->right = NULL;
+
+    return newRoot;
+}
+
+//////////////////////////////////////////////////////////
+
+int main()
+{
+    // 50 20 70 10 30 110 -1
+    Node* root = NULL;
+    cout<<"Enter data to create BST" <<endl;
+    takeInput(root);
+
+    root = twoSumInBST(root);
+    levelOrderTrav(root);
+}
