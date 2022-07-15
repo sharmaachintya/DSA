b5fecedcbcc4119f4f6ac718b2182c87182eb5f8
Achintya Sharma <achintya9998@gmail.com>
2022-07-16 04:13:32 +0530

    Graph

--- Graph/CycleDetectionInDirectedGraph.cpp
+++ Graph/CycleDetectionInDirectedGraph.cpp
@@ -2,6 +2,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/////////////////////////////////////////////////////////////////////
+// Using DFS
+
 bool detectCycle(int node, unordered_map<int, bool> &visited, unordered_map<int, bool> &dfsVisited, unordered_map<int, list<int>> &adj)
 {
     visited[node] = true;
@@ -48,6 +51,71 @@ bool cycleDetection(int n, vector<pair<int, int>> edges)
     }
     return false;
 }
+
+/////////////////////////////////////////////////////////////////////////
+// Using BFS
+
+bool cycleDetection(int n, vector<pair<int, int>> edges)
+{
+    // create adj list
+    unordered_map<int, list<int>> adj;
+    for (int i=0;i<edges.size();i++)
+    {
+        int u = edges[i].first;
+        int v = edges[i].second;
+
+        adj[u].push_back(v);
+    }
+
+    // find all indegree
+    vector<int> indegree(n);
+    for (int i=1;i<=n;i++)
+    {
+        for (auto i : adj)
+        {
+            for (auto j : i.second)
+            {
+                indegree[j]++;
+            }
+        }
+    }
+
+    // push all nodes with 0 indegree in queue
+    queue<int> q;
+    for (int i=1;i<=n;i++)
+    {
+        if (indegree[i] == 0)
+        {
+            q.push(i);
+        }
+    }
+
+    // do BFS
+    int cnt = 0;
+    while (!q.empty())
+    {
+        int front = q.front();
+        q.pop();
+
+        cnt++;
+
+        // check for all neighbours
+        for (auto neighbour : adj[front])
+        {
+            indegree[neighbour]--;
+            if (indegree[neighbour] == 0)
+                q.push(neighbour);
+        }
+    }
+
+    if (cnt != n)
+        return true;
+    else
+        return false;
+}
+
+
+////////////////////////////////////////////////////////////////////////
 
 int main()
 {
