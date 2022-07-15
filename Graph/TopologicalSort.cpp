ab4255f85f7b91fdf360e2cf5d689cea7c512777
Achintya Sharma <achintya9998@gmail.com>
2022-07-15 15:32:19 +0530

    Graph

--- Graph/TopologicalSort.cpp
+++ Graph/TopologicalSort.cpp
@@ -52,6 +52,62 @@ vector<int> topologicalSort(vector<pair<int,int>> edges, int n, int m)
 
 /////////////////////////////////////////////////////////////////////////
 
+// Using Kahn's Algorithm
+
+vector<int> topologicalSort(vector<pair<int,int>> edges, int n, int m)
+{
+    // create adj list
+    unordered_map<int, list<int>> adj;
+    for (int i=0;i<m;i++)
+    {
+        int u = edges[i].first;
+        int v = edges[i].second;
+
+        adj[u].push_back(v);
+    }
+
+    // find all indegrees
+    vector<int> indegree(n);
+    for (auto i : adj)
+    {
+        for (auto j : i.second)
+        {
+            indegree[j]++;
+        }
+    }
+
+    // push nodes with 0 indegree in queue
+    queue<int> q;
+    for (int i=0;i<n;i++)
+    {
+        if (indegree[i] == 0)
+            q.push(i);
+    }
+
+    // do BFS
+    vector<int> ans;
+    while (!q.empty())
+    {
+        int front = q.front();
+        q.pop();
+
+        ans.push_back(front);
+
+        // check for neighbour and update neighbour's indegree
+        for (auto neighbour : adj[front])
+        {
+            indegree[neighbour]--;
+            if (indegree[neighbour] == 0)
+            {
+                q.push(neighbour);
+            }
+        }
+    }
+    return ans;
+}
+
+
+
 int main()
 {
     
