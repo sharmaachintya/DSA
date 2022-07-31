951a69c43d14f05a88bffcaaa5277a6207c49753
Achintya Sharma <achintya9998@gmail.com>
2022-08-01 02:25:25 +0530

    Strings

---
+++ String/LengthOfString.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int getLength(char arr[])
+{
+    int count = 0;
+    for (int i=0;arr[i] != '\0';i++)
+    {  
+        count ++;
+    }
+    return count;
+}
+int main()
+{
+    char name[10];
+
+    cout<<"Enter the name: ";
+    cin>>name;
+
+    cout<<"Length of the name is: "<<getLength(name);
+    
+}
