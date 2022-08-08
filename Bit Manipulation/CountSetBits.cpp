8e86e06147d028b86fe816bac66d8f72b6de018b
Achintya Sharma <achintya9998@gmail.com>
2022-08-08 12:14:19 +0530

    Bit Manipulation

---
+++ Bit Manipulation/CountSetBits.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    uint32_t n = 00000000000000000000000000001011;
+    int count = 0;
+    while (n > 0)    
+    {
+        if (n % 2 == 1)
+        {
+            count++;
+        }
+        n = n / 2;
+    }
+    cout<<count<<endl;
+}
