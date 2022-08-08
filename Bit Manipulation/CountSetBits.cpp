f6bf3f12f0edaebc31cfd87dcc4a34a3faf1f117
Achintya Sharma <achintya9998@gmail.com>
2022-08-08 12:17:15 +0530

    Bit Manipulation

--- Bit Manipulation/CountSetBits.cpp
+++ Bit Manipulation/CountSetBits.cpp
@@ -7,11 +7,12 @@ int main()
     int count = 0;
     while (n > 0)    
     {
-        if (n % 2 == 1)
+        if (n & 1)
         {
             count++;
         }
-        n = n / 2;
+        //n = n / 2;
+        n = n >> 1;
     }
     cout<<count<<endl;
 }
