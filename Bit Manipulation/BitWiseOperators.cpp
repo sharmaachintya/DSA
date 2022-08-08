a1eb9e21d8864094b535058cd54d92af3e015a55
Achintya Sharma <achintya9998@gmail.com>
2022-08-08 11:48:50 +0530

    Bit Manipulation

---
+++ Bit Manipulation/BitWiseOperators.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a = 4;
+    int b = 6;
+
+    cout<<"a & b -> "<<(a&b)<<endl;
+    cout<<"a | b -> "<<(a|b)<<endl; 
+    cout<<" ~a -> "<<~a<<endl; 
+    cout<<"a ^ b -> "<<(a^b)<<endl; 
+
+    cout<<(5<<1)<<endl;
+    cout<<(3<<2)<<endl;
+    cout<<(5>>2)<<endl;
+
+}
