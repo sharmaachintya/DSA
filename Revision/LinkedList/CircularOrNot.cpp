eb154f6636cef2346d8f2cba2e3b1ab266ee6acc
Achintya Sharma <achintya9998@gmail.com>
2022-08-17 19:10:47 +0530

    Revision

--- Revision/LinkedList/CircularOrNot.cpp
+++ Revision/LinkedList/CircularOrNot.cpp
@@ -1,7 +1,50 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+class Node{
+    public: 
+        int data;
+        Node *next;
+
+        // Constructor
+        Node(int data)
+        {
+            this->data = data;
+            this-> next = NULL;
+        }
+        // Destructor
+        ~Node()
+        {
+            int value  = this->data;
+            if(this->next != NULL)              // Memory Free
+            {
+                delete next;
+                this->next = NULL;
+            }
+            cout<<"Memory Freed for Node with data "<<this->data<<endl;
+        }
+};
+
+bool circularOrNot(Node* head)
+{
+    if (head == NULL)
+        return true;
+
+    if (head->next == NULL)
+        return false;
+
+    Node* temp = head->next;
+    while (temp != NULL || temp != head)
+    {
+        temp = temp->next;
+    }
+    if (temp == head)
+        return true;
+    
+    return false;
+}
+
 int main()
 {
-        
+
 }
