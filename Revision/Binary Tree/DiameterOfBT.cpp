aec7c3e816133caefece2563a07cb321cba235c3
Achintya Sharma <achintya9998@gmail.com>
2022-08-22 01:17:55 +0530

    Revision

---
+++ Revision/Binary Tree/DiameterOfBT.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int d)
+        {
+            this->data = d;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+Node* buildTree(Node* root)
+{
+    cout<<"Enter the data: "<<endl;
+    int data;
+    cin>>data;
+    root = new Node(data);
+
+    if (data == -1)
+    {
+        return NULL;
+    }
+
+    cout<<"Enter data for left node of "<<data<<endl;
+    root->left = buildTree(root->left);
+    cout<<"Enter data for right node of "<<data<<endl;
+    root->right = buildTree(root->right);
+    return root;
+}
+
+
+int diameterOfBT(Node* root)
+{
+    
+}
+
+int main()
+{
+
+}
