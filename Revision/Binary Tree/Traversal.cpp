f7cc13c2f6c7e4bb9ed6739d5c3504dd40294407
Achintya Sharma <achintya9998@gmail.com>
2022-08-21 21:29:49 +0530

    Revision

---
+++ Revision/Binary Tree/Traversal.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int d)
+        {
+            this->data = d;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+Node* buildTree(Node* root)
+{
+    cout<<"Enter the data: "<<endl;
+    int data;
+    cin>>data;
+    root = new Node(data);
+
+    if (data == -1)
+    {
+        return NULL;
+    }
+
+    cout<<"Enter data for left node of "<<data<<endl;
+    root->left = buildTree(root->left);
+    cout<<"Enter data for right node of "<<data<<endl;
+    root->right = buildTree(root->right);
+    return root;
+}
+
+void levelOrderTraversal(Node* root)
+{
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty())
+    {
+        Node* front = q.front();
+        q.pop();
+
+        if (front == NULL)
+        {
+            cout<<endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout<<front->data<<" ";
+            if (front->left)
+            {
+                q.push(front->left);
+            }
+            if (front->right)
+            {
+                q.push(front->right);
+            }
+        }
+    }
+}
+
+
+
+int main()
+{
+    
+}
