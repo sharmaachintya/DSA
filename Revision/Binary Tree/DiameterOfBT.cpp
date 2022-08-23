4de10cc3ff28e8388dc1c765e07884cd9a42506c
Achintya Sharma <achintya9998@gmail.com>
2022-08-23 21:36:10 +0530

    Revision

--- Revision/Binary Tree/DiameterOfBT.cpp
+++ Revision/Binary Tree/DiameterOfBT.cpp
@@ -36,9 +36,32 @@ Node* buildTree(Node* root)
 }
 
 
-int diameterOfBT(Node* root)
+pair<int, int> fastDia(Node* root)
 {
     
+    if (root == NULL)
+    {
+        pair<int, int> p = make_pair(0,0);
+        return p;
+    }
+
+    pair<int, int> left = fastDia(root->left);
+    pair<int, int> right = fastDia(root->right);
+
+    int op1 = left.first;
+    int op2 = right.first;
+    int op3 = left.second + right.second + 1;
+
+    pair<int, int> ans;
+    ans.first = max(op1, max(op2, op3));
+    ans.second = max(left.second, right.second) + 1;
+
+    return ans;
+}
+
+int diameterOfBT(Node* root)
+{
+    return fastDia(root).first;
 }
 
 int main()
