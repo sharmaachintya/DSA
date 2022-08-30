d77785e1e6f7abb434ed59b1b1f36772a96cf89b
Achintya Sharma <achintya9998@gmail.com>
2022-08-30 21:46:38 +0530

    Sorting

--- Revision/Sorting/BubbleSort.cpp
+++ Revision/Sorting/BubbleSort.cpp
@@ -8,11 +8,17 @@ int main()
 
     for (int i=0;i<n-1;i++)
     {
+        bool swapped = false;
         for (int j=0;j<n-i;j++)
         {
             if (arr[j] > arr[j+1])
+            {
                 swap(arr[j], arr[j+1]);
+                swapped = true;
+            }   
         }
+        if (swapped == false)
+            break;
     }
     for (auto i : arr)    
     {
