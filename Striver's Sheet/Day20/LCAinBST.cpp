695e6b53491a3937b913bb43699f35accf6fe8b2
Achintya Sharma <achintya9998@gmail.com>
2022-08-31 15:34:21 +0530

    Binary Search Tree - I

---
+++ Striver's Sheet/Day20/LCAinBST.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int d)
+        {
+            this->data = d;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+Node* insertIntoBST(Node* &root, int d)
+{
+    if (root == NULL)
+    {
+        root = new Node(d);
+        return root;
+    }
+    if (d > root->data)
+    {
+        root->right = insertIntoBST(root->right, d);
+    }
+    else
+    {
+        root->left = insertIntoBST(root->left, d);
+    }
+    return root;
+}
+
+void takeInput(Node* &root)
+{
+    int data;
+    cin>>data;
+
+    while (data != -1)
+    {
+        insertIntoBST(root,data);
+        cin>>data;
+    }
+}
+
+Node* lcaInBST(Node* p, Node* q, Node* root)
+{
+    if (root == NULL)
+        return NULL;
+    
+    while (root != NULL)
+    {
+        if (root->data > p->data && root->data > q->data)
+            root = lcaInBST(p, q, root->left);
+        if (root->data < p->data && root->data < q->data)
+            root = lcaInBST(p, q, root->right);
+        else
+            return root;
+    }
+    return NULL;
+}
+
+
+int main()
+{
+    // 50 20 70 10 30 110 -1
+    Node* root = NULL;
+    cout<<"Enter data to create BST" <<endl;
+    takeInput(root);
+
+
+}
