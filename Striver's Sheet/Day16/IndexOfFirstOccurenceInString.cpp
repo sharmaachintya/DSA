6b564af0e08adcaadc5d479cca61d6c8deafd7ad
Achintya Sharma <achintya9998@gmail.com>
2022-09-12 16:04:34 +0530

    Strings - II

--- Striver's Sheet/Day16/IndexOfFirstOccurenceInString.cpp
+++ Striver's Sheet/Day16/IndexOfFirstOccurenceInString.cpp
@@ -2,6 +2,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// In Case Z function just make an integer count and increment it, if you'll find the string
+
 int IndexofFirstOccur(string needle, string haystack)
 {
     int n = needle.size();
