5df355a47e29de96664ef7f22eaa86223b9a4d93
Achintya Sharma <achintya9998@gmail.com>
2022-09-13 18:27:42 +0530

    Arrays

---
+++ Array/MoveAllTheNegToOneSide.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> arr = {1, -1, 3, 2, -7, -5, 11, 6};
+    vector<int> ans;
+    for (int i=0;i<n;i++)
+    {
+        if (arr[i] > 0)
+            ans.push_back(arr[i]);
+    }
+    for (int i=0;i<n;i++)
+    {
+        if (arr[i] < 0)
+            ans.push_back(arr[i]);
+    }    
+    for(auto i : ans)
+        cout<<i<<" ";
+}
