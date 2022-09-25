0a1bf3bf2ab477466f2b892e0f606b37d3e09683
Achintya Sharma <achintya9998@gmail.com>
2022-09-25 14:18:37 +0530

    Arrays - III

--- Array/PowXN.cpp
+++ Array/PowXN.cpp
@@ -1,7 +1,34 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+
+int powXN(int x, int n, int m)
+{
+    int ans = 1;
+    long long nn = n;
+    long long xx = x;
+    if (nn < 0)
+        nn = -1 * nn;
+    while (nn)
+    {
+        if (nn % 2)
+        {
+            ans = (ans * xx) % m;
+            nn = nn - 1;
+        }
+        else
+        {
+            xx = (xx) * (xx) % m;
+            nn = nn / 2;
+        }
+    }
+    return ans;
+}
+
 int main()
 {
-        
+    int x = 3;
+    int n = 1;
+    int m = 2; 
+    cout<<powXN(x, n, m);
 }
