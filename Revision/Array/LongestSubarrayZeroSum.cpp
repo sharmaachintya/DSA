94730177dfc7148987ae554ae8570d429678ed5d
Achintya Sharma <achintya9998@gmail.com>
2023-05-24 13:29:32 +0530

    Array Revision

---
+++ Revision/Array/LongestSubarrayZeroSum.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int> &arr)
+{
+    int sum = 0, maxi = 0;
+    unordered_map<int, int> mp;
+    for (int i=0;i<arr.size();i++)
+    {
+        sum += arr[i];
+        if (sum == 0)
+            maxi = i + 1;
+        else
+        {
+            if (mp.find(sum) != mp.end())
+                maxi = max(maxi, i - mp[sum]);
+            else
+                mp[sum] = i;
+        }
+    }
+    return maxi;
+}
+
+int main()
+{
+    vector<int> arr = {1, 3, -1, 4, -4};
+    cout<<solve(arr);
+}
