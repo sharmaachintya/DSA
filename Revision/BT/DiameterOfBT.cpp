57e75bce07d5240f3caa270705bb8bcf60e01c2e
Achintya Sharma <achintya9998@gmail.com>
2023-06-16 13:30:08 +0530

    BT Revision :)

---
+++ Revision/BT/DiameterOfBT.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int data)
+        {
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+Node* buildTree(Node* root)
+{
+    cout<<"Enter the Data: "<<endl;
+    int data;
+    cin>>data;
+    root = new Node(data);
+
+    if (data == -1)
+        return NULL;
+
+    cout<<"Enter the data for left Node of "<<data<<endl;
+    root->left = buildTree(root->left);
+    cout<<"Enter the data for right Node of "<<data<<endl;
+    root->right = buildTree(root->right);
+    return root;
+}
+
+pair<int, int> fastDia(Node *root)
+{
+    if (root == NULL)
+    {
+        pair<int, int> p = make_pair(0, 0);
+        return p;
+    }
+
+    pair<int, int> left = fastDia(root->left);
+    pair<int, int> right = fastDia(root->right);
+
+    int op1 = left.first;
+    int op2 = right.first;
+    int op3 = left.second + right.second + 1;
+
+    pair<int, int> ans;
+    ans.first = max(op1, max(op2, op3));
+    ans.second = max(left.second, right.second) + 1;
+
+    return ans;
+}
+
+int diameterBT(Node* root)
+{
+    return fastDia(root).first;
+}
+
+int main()
+{
+    Node* root = NULL;
+    root = buildTree(root);
+    //1 3 7 8 -1 -1 -1 -1 11 -1 -1 5 17 -1 -1 -1
+
+    cout<<"Diameter is: "<<diameterBT(root);
+}
---
+++ Revision/BT/HeightOfBT.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int data)
+        {
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+Node* buildTree(Node* root)
+{
+    cout<<"Enter the Data: "<<endl;
+    int data;
+    cin>>data;
+    root = new Node(data);
+
+    if (data == -1)
+        return NULL;
+
+    cout<<"Enter the data for left Node of "<<data<<endl;
+    root->left = buildTree(root->left);
+    cout<<"Enter the data for right Node of "<<data<<endl;
+    root->right = buildTree(root->right);
+    return root;
+}
+
+int heightBT(Node* root)
+{
+    if (root == NULL)
+        return 0;
+    int h1 = heightBT(root->left);
+    int h2 = heightBT(root->right);
+
+    int ans = max(h1, h2) + 1;
+    return ans;
+}
+
+int main()
+{
+    Node* root = NULL;
+    root = buildTree(root);
+    //1 3 7 8 -1 -1 -1 -1 11 -1 -1 5 17 -1 -1 -1
+
+    cout<<"height is: "<<heightBT(root);
+}
