ce393f5d5ccfb3fe38f7a92e61c6699cd0846ee7
Achintya Sharma <achintya9998@gmail.com>
2023-06-22 14:15:23 +0530

    BT Revision :)

---
+++ Revision/BT/MaxSumOfNonAdjacent.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int data)
+        {
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+Node* buildTree(Node* root)
+{
+    cout<<"Enter the Data: "<<endl;
+    int data;
+    cin>>data;
+    root = new Node(data);
+
+    if (data == -1)
+        return NULL;
+
+    cout<<"Enter the data for left Node of "<<data<<endl;
+    root->left = buildTree(root->left);
+    cout<<"Enter the data for right Node of "<<data<<endl;
+    root->right = buildTree(root->right);
+    return root;
+}
+
+pair<int, int> solve(Node* root)
+{
+    if (root == NULL)
+    {
+        pair<int, int> p = make_pair(0, 0);
+        return p;
+    }
+
+    pair<int, int> left = solve(root->left);
+    pair<int, int> right = solve(root->right);
+
+    pair<int, int> ans;
+
+    ans.first = root->data + left.second + right.second;
+    ans.second = max(left.first, left.second) + max(right.first, right.second);
+
+    return ans;
+}
+
+int maxSumNonAdj(Node* root)
+{
+    pair<int, int> ans = solve(root);
+    return max(ans.first, ans.second);
+}
+
+int main()
+{
+    Node* root = NULL;
+    root = buildTree(root);
+    //1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+
+    cout<<maxSumNonAdj(root);
+}
